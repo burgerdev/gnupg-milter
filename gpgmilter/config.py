ae1828379ad1d00318538b7c6ee6d3917844decb
Markus Döring <webmaster@burgerdev.de>
2013-12-12 22:20:17 +0100

    implemented config parser

--- gpgmilter/config.py
+++ gpgmilter/config.py
@@ -1,4 +1,7 @@
 
+import ConfigParser
+import StringIO
+
 import logging
 
 logger = logging.getLogger("gnupg-milter")
@@ -7,40 +10,48 @@ logging.basicConfig()
 from logging import DEBUG, INFO, WARN, ERROR
 loglevel = [DEBUG, INFO, WARN, ERROR]
 
+_defaults = {
+    'message': """This mail has been automatically encrypted at your ISP.""",
+    'socket': "unix:/var/spool/postfix/var/spool/gnupg-milter/milter.socket",
+    'timeout': "600",
+    'gnupghome': "",
+    }
+
+section = 'gnupg-milter'
+
+
 class Config(object):
     '''
     A configuration for the gnupg-milter.
     '''
-
-    # message to add to the email body (None for no appendend message)
-    #TODO add warnings
-    msg = """This mail has been automatically encrypted at your ISP."""
-
-    # pipe file to use for message passign between sendmail and gnupg-milter
-    socket = "unix:/var/spool/postfix/var/spool/gnupg-milter/milter.socket"
-
-    # abort milter actions after no input for x seconds
-    #FIXME is that so?
-    timeout = 600
-
-    # GPG home directory (where the key rings roam)
-    #FIXME don't use temp!!!
-    gnupghome = "/tmp/gpgmilter"
-
-    def init_config(self, filename=None):
+    
+    def __init__(self, filename=None):
         '''
         Read the configuration file and store results in this
          object.
         If filename is None, the default configuration will 
         be used.
         '''
-        #TODO read real config file
-        return
+
+        self.parser = ConfigParser.SafeConfigParser(_defaults)
+        self.parser.add_section(section)
+
+        if filename is not None:
+            self.parser.read([filename])
+        
+        for key in _defaults.keys():
+            setattr(self, key, self.parser.get(section, key))
 
     def log(self, msg, level=INFO):
         #TODO implement syslog
         #TODO static or per-object?
         assert level in loglevel, "Invalid logging level"
-        print("BLA")
         logger.log(level, msg)
+    
+    def __str__(self):
+        s = StringIO.StringIO("")
+        s.write("<{}.Config> object:\n".format(__name__))
+        for attr in _defaults.keys():
+            s.write("\t{} = {}\n".format(attr, getattr(self,attr)))
+        return s.getvalue()
 
--- test/testConfig.py
+++ test/testConfig.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env nosetests
 
+import unittest
+import sys
+import os
 
+# include the top directory in the search path, if the module is not yet added
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.abspath(os.path.join(current_dir, "..")))
+
+import gpgmilter
+
+class TestConfig(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+    
+    def testStorage(self):
+        fn = os.path.abspath(os.path.join(current_dir, "..", "config", "gnupg-milter.conf"))
+        conf = gpgmilter.config.Config(filename=fn)
+        assert hasattr(conf, "gnupghome")
